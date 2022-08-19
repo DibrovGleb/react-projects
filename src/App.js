4bb60c8c46d92b03efd3711162be0e51ca56ae67
Gleb <g.t.ru@mail.ru>
2022-08-19 16:39:51 +0300

    deleted readme and update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.sass';
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
+
+const imgArr = require.context('./pokemons', false, /\.(png|svg)$/),
+      images = imgArr.keys().map(imgArr)
+
+const CreateImage=()=> {
+  let randomIndex = Math.floor(Math.random() * 11)
+  return <img src={images[randomIndex]} alt="" />
+}
 
 function App() {
   const [count, setCount] = useState(0),
+        [imagesList, setInputList] = useState([]),
         onClickPlus = ()=>{
           setCount(count + 1)
+          setInputList(imagesList.concat(<CreateImage key={imagesList.length} />))
         },
-        onClickMinus = ()=>{ if(count >0) {setCount(count - 1)} }
+        onClickMinus = ()=>{ 
+          if(count >0) setCount(count - 1); setInputList(imagesList.slice(0, imagesList.length - 1))
+        }
+
   return (
     <div className="App">
       <h2>Counter</h2>
@@ -15,7 +28,11 @@ function App() {
         <button onClick={onClickMinus} className='minus'>- Minus</button>
         <button onClick={onClickPlus} className='plus'>+ Plus</button>
       </div>
+      <div className="images-container">
+        {imagesList}
+      </div>
     </div>
+    
   );
 }
 
