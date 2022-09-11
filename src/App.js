f842e0bc319b38395355efa497cbcbb98503a390
Gleb <g.t.ru@mail.ru>
2022-09-11 10:41:37 +0300

    quiz

--- src/App.js
+++ src/App.js
@@ -9,31 +9,12 @@ const CreateImage=()=> {
   return <img src={images[randomIndex]} alt="" />
 }
 
-function App() {
-  const [count, setCount] = useState(0),
-        [imagesList, setInputList] = useState([]),
-        onClickPlus = ()=>{
-          setCount(count + 1)
-          setInputList(imagesList.concat(<CreateImage key={imagesList.length} />))
-        },
-        onClickMinus = ()=>{ 
-          if(count >0) setCount(count - 1); setInputList(imagesList.slice(0, imagesList.length - 1))
-        }
+export default function App() {
 
   return (
     <div className="App">
-      <h2>Counter</h2>
-      <h1>{count}</h1>
-      <div className="buttons">
-        <button onClick={onClickMinus} className='minus'>- Minus</button>
-        <button onClick={onClickPlus} className='plus'>+ Plus</button>
-      </div>
-      <div className="images-container">
-        {imagesList}
-      </div>
+
     </div>
     
   );
-}
-
-export default App;
+};
