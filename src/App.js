63d0ae7813cf0b29617ed633fff35613f128d8b2
Gleb <g.t.ru@mail.ru>
2022-09-29 15:06:01 +0300

    new project upload on new branch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import './App.sass'
 import { useState } from 'react'
 import React from 'react'
-import questions from './components/questions'
-import Result from './components/Result'
-import Game from './components/Game'
+
 
 export default function App() {
-  const [step, setStep] = useState(0),
-        [correct, setCorrect] = useState(0),
-        question = questions[step],
-        onClickAnswer = (index) =>{
-          setTimeout(() => {
-            setStep(step + 1)
-            if(index === question.correct) setCorrect(correct + 1)
-        }, 100)},
-        onClickReset = ()=>{
-            setStep(0)
-            setCorrect(0)
-        }
 
   return (
     <div className="App">
-      {
-        step !== questions.length 
-        ? <Game step={step} question={question} questions={questions} onClickAnswer={onClickAnswer}/> 
-        : <Result correct={correct} questions={questions} onClickReset={onClickReset}/>
-      } 
+        template for new project
     </div>
   )
 }
